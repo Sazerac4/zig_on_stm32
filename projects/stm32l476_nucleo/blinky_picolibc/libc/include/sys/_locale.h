04e9d892a1bf101d4deb3e27200aa537ca902f94
Sazerac4 <lm-embeddedsystems@laposte.net>
2025-02-05 09:07:49 +0100

    Add exemple with custom libc compilation 'picolibc'

---
+++ projects/stm32l476_nucleo/blinky_picolibc/libc/include/sys/_locale.h
@@ -0,0 +1,10 @@
+/* Copyright (c) 2016 Corinna Vinschen <corinna@vinschen.de> */
+/* Definition of opaque POSIX-1.2008 type locale_t for userspace. */
+
+#ifndef	_SYS__LOCALE_H
+#define _SYS__LOCALE_H
+
+struct __locale_t;
+typedef struct __locale_t *locale_t;
+
+#endif	/* _SYS__LOCALE_H */
